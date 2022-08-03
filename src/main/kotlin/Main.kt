2a05e6a67c58bcdd5504e81f086aebfe80034dde
Бородинский Алексей <a-borodinskiy@mail.ru>
2022-08-03 21:13:55 +0300

    Готовое

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,7 +2,7 @@ package ru.netology
 fun main() {
     val musicLover = true
     val sum = 10000
-    val discount = if (sum >10000) {
+    val discount = if (sum > 10000) {
         sum - (sum / 100 * 5)
     } else if (sum >= 1001 && sum <= 10000) {
         sum - 100
